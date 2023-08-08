d7a0b59b0c79ec2bc8f9a090a0bbc6a814554a0b
AstasiaG <anstasa.li@gmail.com>
2023-08-08 12:54:36 +0400

    feat: add menu class

---
+++ menu.js
@@ -0,0 +1,17 @@
+export class menu {
+  constructor (arr) {
+      this.moves = [...arr];
+  }
+
+  showMenu () {
+      console.log('Available moves:');
+      this.moves.forEach((e,i) => console.log(`${i+1} - ${e}`));
+      console.log('0 - exit');
+      console.log('? - help');
+  };
+
+  showMoves (pc,user, moves) {
+      console.log(`Your move: ${moves[user - 1]}`);
+      console.log(`Computer move: ${pc}`);
+  }
+}
