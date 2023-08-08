08fe07a2645f0db4572d4ea436151a353cd6d9e7
AstasiaG <anstasa.li@gmail.com>
2023-08-08 13:11:22 +0400

    feat: add keyGen class

---
+++ keyGen.js
@@ -0,0 +1,20 @@
+const crypto = await import('node:crypto');
+const { generateKeySync } = import ('node:crypto');
+const { createHmac } = await import('node:crypto');
+
+export class Hmac {
+  keyGen () {
+    this.key = crypto.generateKeySync('hmac', { length: 256 }).export().toString('hex');
+  }
+
+  hmacGen (pcMove) {
+    this.hmac = createHmac('sha256', this.key)
+    .update(pcMove)
+    .digest('hex');
+    console.log(`HMAC: ${this.hmac}`);
+  }
+
+  showKey () {
+    console.log(`HMAC key: ${this.key}`);
+  }
+}
